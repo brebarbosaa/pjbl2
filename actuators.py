a408689c655820bfe5bdc3e5f058ad1ffa3432eb
Brenda Barbosa <barbosa.brb@outlook.com>
2025-05-11 00:03:03 -0300

    alterações
    alterei algumas variaveis e funções que tavam dando conflito e adicionei CSS e algumas outras mudanças de estilo

--- actuators.py
+++ actuators.py
@@ -3,15 +3,15 @@ from decorators import admin_required
 
 actuators = Blueprint("actuators", __name__, template_folder="templates")
 
-# Lista de atuadores iniciais (exemplo de status 1 ou 0 para "ligado" ou "desligado")
-atuadores = {
+dicAtuadores = {
         'Servo Motor': 'Desligado',
         'LEDs': 'Ligados',
         'Cooler': 'Desligado',
-    }
+}
+# Lista de atuadores iniciais (exemplo de status 1 ou 0 para "ligado" ou "desligado")
 @actuators.route('/dashboard')
 def dashboard():
-    return render_template('dashboard_atuadores.html', atuadores=atuadores)
+    return render_template('dashboard_atuadores.html', atuadores=dicAtuadores)
 
 @actuators.route('/register_actuator')
 @admin_required
@@ -21,35 +21,31 @@ def register_actuator():
 @actuators.route('/add_actuator', methods=['POST'])
 @admin_required
 def add_actuator():
-    global atuadores
     if request.method == 'POST':
         atuador = request.form['atuador']
         valor = request.form['valor']
     else:
         atuador = request.args.get('atuador', None)
         valor = request.args.get('valor', None)
-    atuadores[atuador] = valor
-    return render_template('actuators.html', devices=atuadores)
+    dicAtuadores[atuador] = valor
+    return render_template('actuators.html', devices=dicAtuadores)
 
 @actuators.route('/remove_actuator')
 @admin_required
 def remove_actuator():
-    global atuadores
-    return render_template('remove_actuator.html', devices=atuadores)
+    return render_template('remove_actuator.html', devices=dicAtuadores)
 
 @actuators.route('/del_actuator', methods=['GET', 'POST'])
 @admin_required
 def del_actuator():
-    global atuadores
     if request.method == 'POST':
         atuador = request.form['atuador']
     else:
         atuador = request.args.get('atuador', None)
-    if atuador in atuadores:
-        atuadores.pop(atuador)
-    return render_template('actuators.html', devices=atuadores)
+    if atuador in dicAtuadores:
+        dicAtuadores.pop(atuador)
+    return render_template('actuators.html', devices=dicAtuadores)
 
 @actuators.route('/actuators')
-def ver_atuadores():
-    global atuadores
-    return render_template('actuators.html', devices=atuadores)
+def visualizar_atuadores():
+    return render_template('actuators.html', devices=dicAtuadores)
