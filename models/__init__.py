d0e59bc4ce8db3eb9fca1e1ab76e962b2a3cc85d
natalia-trevisol <nataliamtrevisol@yahoo.com.br>
2025-05-20 10:53:32 -0300

    criacao banco de dados

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+from models.db import db
+#from models.iot.devices import Device
+#from models.iot.sensors import Sensor
+#from models.iot.actuators import Actuator
+#from models.iot.read import Read
+#from models.iot.write import Write
---
+++ models/db.py
@@ -0,0 +1,5 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+instance = 'mysql+pymysql://userRoot:userRoot@localhost:3306/cavalo'
